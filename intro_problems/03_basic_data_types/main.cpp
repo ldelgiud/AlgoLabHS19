4cd7bb33330335eb6403a74f362c01b6b32effea
Leonardo Del_Giudice <ldelgiud@student.ethz.ch>
2019-09-17 15:15:36 +0200

    task 03

---
+++ intro_problems/03_basic_data_types/main.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include <iomanip>      // std::setprecision
+
+using namespace std;
+
+void test_case() {
+  int a;
+  long long b;
+  string  c;
+  double d;
+
+  cin >> a >> b >> c >> d;
+  
+  cout << a << ' ' << b << ' ' << c << ' ' << d << endl;
+  
+}
+
+
+int main() {
+  
+  int t; cin >> t;
+  std::cout << std::setprecision(3);
+  while(t--) {
+    test_case();
+
+  }
+}
