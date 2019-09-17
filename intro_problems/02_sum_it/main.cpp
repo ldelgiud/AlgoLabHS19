e4b0486b1da52251fe303968726ea1dab2a2e34f
Leonardo Del_Giudice <ldelgiud@stud19-h57.inf.ethz.ch>
2019-09-17 11:37:28 +0200

    start 02

---
+++ intro_problems/02_sum_it/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int t;
+  cin >> t;
+
+  while (t--) {
+    int sum = 0;
+    int n;
+    cin >> n;
+
+    while (n--) {
+      int temp;
+      cin >> temp;
+      sum += temp;
+    }
+
+    cout << sum << endl;
+  }
+}
