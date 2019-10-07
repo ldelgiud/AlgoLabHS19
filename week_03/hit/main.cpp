861b1823068017ac44d16850da7bbaddba479af1
Leonardo Del_Giudice <ldelgiud@student.ethz.ch>
2019-10-07 13:54:36 +0200

    hit 100 points

--- week_03/hit/main.cpp
+++ week_03/hit/main.cpp
@@ -1,13 +1,36 @@
 #include <iostream>
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
 
 using namespace std;
+typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
+bool test_case(int n) {
 
-void test_case() {
+  long a, b, x, y; cin >> x >> y >> a >> b;
 
+  K::Point_2 start(x,y), direction(a,b);
+  K::Ray_2 ray(start, direction);
+  bool intersect = false;
+  for (int i = 0; i < n; ++i) {
+    long r, s, t, u;
+    cin >> r >> s >> t >> u;
+    
+    K::Point_2 begin(r,s), end(t,u);
+    K::Segment_2 segment(begin,end);
+
+    if (!intersect && CGAL::do_intersect(ray, segment)) {
+      intersect = true;
+    }
+  }
+
+  return intersect;
 }
 
 int main() {
-  int t; cin >> t;
-
-  while(t--) test_case();
+  int n;
+  cin >> n;
+  while(n) {
+    if (test_case(n)) cout << "yes\n";
+    else cout << "no\n";
+    cin >> n;
+  }
 }
