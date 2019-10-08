1730c42c2c07efe0d2249f6339e45ef5e4b31326
Leonardo Del_Giudice <ldelgiud@student.ethz.ch>
2019-10-08 14:29:39 +0200

    chariot_race 20 points

--- potw_03/main.cpp
+++ potw_03/main.cpp
@@ -2,7 +2,12 @@
 #include <vector>
 #include <utility>
 #include <set>
+
 using namespace std;
+
+// Used for debugging
+#define step(x) cout << "Step (" << x << ")" << endl
+#define trace(x) cout << #x << " = " << x << endl
 
 vector<vector<int>> G;
 int n;
@@ -40,36 +45,57 @@ void test_case() {
   //init DP
   DP.clear();
   DP = vector<pair<int,bool>>(n, make_pair(-1,false));
+
+  //Leaves
   for (int i: finish) {
     DP[i] = make_pair(C[i], true);
   }
-
+  
+  //Parents of leaves
+  for (int i = 0; i < n; ++i) {
+    bool flag = true;
+    int cost = 0;
+    for (int v: G[i]) {
+      cost += C[v];
+      if (G[v].size() > 0) flag = false;
+    }
+    if (flag && DP[i].first == -1) {
+      if (cost >= C[i]) {
+	DP[i].first = C[i];
+	DP[i].second = true;
+      } else {
+        DP[i].first = cost;
+	DP[i].second = false;
+      }
+    }
+  }
+  
   int res = play(0);
   cout << res << endl;
+  /*
+  for (auto it: DP) {
+    cout << it.first << ' ';
+  }*/
 }
 
 int play(int node) {
-  //cout << "node " << node << endl; 
-  if (G[node].size() == 0) return C[node];
-
+  
   if (DP[node].first == -1) {
     int option1 = C[node];
     for (int v: G[node]) {
-      //cout << "v " << v << endl;
       if (G[v].size() != 0){
-	//cout << "size " << G[v].size() << endl;
 	for (int u: G[v]) {
-	  //cout << "u " << u << endl;
 	  option1 += play(u);
 	}
       } 
     }
+    
     int option2 = 0;
     bool flag = false;
     int minCost = 1 << 28;
     for (int v: G[node]) {
-      if (DP[node].second) flag = true;
       option2 += play(v);
+      if (DP[v].second) flag = true;     
       if (minCost > C[v]) minCost = C[v];
     }
 
@@ -85,11 +111,10 @@ int play(int node) {
   }
   
   return DP[node].first;
-
-
 }
 
 int main() {
+  std::ios_base::sync_with_stdio(false);
   int t; cin >> t;
   while(t--) test_case();
 }
