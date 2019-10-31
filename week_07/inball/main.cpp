7063a9d7f2348f393fe04839da9da9c722ff1cd4
Leonardo Del_Giudice <ldelgiud@student.ethz.ch>
2019-10-31 15:13:33 +0100

    start inball

---
+++ week_07/inball/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+
+
+
+int main() {
+  std::ios_base::sync_with_stdio(false);
+  int n,d;
+
+  while(true) {
+    std::cin >> n;
+    if (!n) break;
+    std::cin >> d;
+
+    
+
+  }
+}
