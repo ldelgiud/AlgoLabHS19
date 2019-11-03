37d4e0ed4a701dc75b4397664b13e9704fe862bc
ldelgiud <leonardo.delgiudice@yahoo.it>
2019-11-03 16:35:25 +0100

    russia 100 points (optimized it. solution)

--- potw_02/main.cpp
+++ potw_02/main.cpp
@@ -3,42 +3,40 @@
 
 using namespace std;
 
-#define trace(x)// cout << #x << ": " << x << endl
+#define trace(x) // cout << #x << ": " << x << endl
 
-int play(int l, int r, int turn);
-vector<int> Coins;
-vector<vector<int>> DP;
+//int play(int l, int r, int turn);
 int n, m, k;
 
 void test_case() {
+  // Read Input
   cin >> n >> m >> k;
-  Coins = vector<int>(n);
-  trace(n);
+  vector<int> Coins;
+  Coins.reserve(n);
   for (int i = 0; i < n; ++i) {
-    cin >> Coins[i];
+    int tmp; cin >> tmp;
+    Coins.push_back(tmp);
   }
 
-  DP = vector<vector<int>>(n,vector<int>(n,0));
+  vector<vector<int>> DP(n,vector<int>(n));
 
-  for (int len = 0; len < n; ++len) {
-    for(int row = 0; row < n; ++row) {
-      int l = row;
-      int r = row + len;
-      if (r >= n) continue;
+  //Initialize DP
+  if (((n-1)%m) == (k%m)) {
+    for (int i = 0; i < n; ++i) {
+      DP[i][i] = Coins[i];
+    }
+  } else {
+    for (int i = 0; i < n; ++i) {
+      DP[i][i] = 0;
+    }
+  }
 
-      int turn = (l + (n-r-1))%m;
-      trace(len);
-      trace(row);
-      if (l==r && turn == k){
-        DP[l][r] = Coins[l];
-        continue;
-      }
-      else if (l==r) {
-        DP[l][r] = 0;
-        continue;
-      }
+  //Calculate entries
+  for (int len = 1; len < n; ++len) {
+    for(int l = n-1-len; l >= 0; --l) {
+      int r = l + len;
 
-      if (turn == k) {
+      if (((l + (n-r-1))%m) == k) {
         DP[l][r] = max(DP[l+1][r] + Coins[l], DP[l][r-1] + Coins[r]);
       } else {
         DP[l][r] = min(DP[l+1][r], DP[l][r-1]);
@@ -46,38 +44,37 @@ void test_case() {
     }
   }
 
-  int result = DP[0][n-1];
-  cout << result << endl;
+  cout << DP[0][n-1] << endl;
 }
 
-int play(int l, int r, int turn) {
-
-  turn = (l + (n-r-1))%m;
-  if (l==r && turn == k) return Coins[l];
-  else if (l==r) return 0;
-  int result;
-
-  if (turn == k) {
-    result = DP[l][r];
-    if (result == -1) {
-      DP[l][r] = max(
-        play(l+1, r, (turn+1)%m) + Coins[l],
-        play(l, r-1, (turn+1)%m) + Coins[r]
-      );
-      result = DP[l][r];
-    }
-  } else {
-    result = DP[l][r];
-    if (result == -1) {
-      DP[l][r] = min(
-        play(l+1, r, (turn+1)%m),
-        play(l, r-1, (turn+1)%m)
-      );
-      result = DP[l][r];
-    }
-  }
-  return result;
-}
+// int play(int l, int r, int turn) {
+//
+//   turn = (l + (n-r-1))%m;
+//   if (l==r && turn == k) return Coins[l];
+//   else if (l==r) return 0;
+//   int result;
+//
+//   if (turn == k) {
+//     result = DP[l][r];
+//     if (result == -1) {
+//       DP[l][r] = max(
+//         play(l+1, r, (turn+1)%m) + Coins[l],
+//         play(l, r-1, (turn+1)%m) + Coins[r]
+//       );
+//       result = DP[l][r];
+//     }
+//   } else {
+//     result = DP[l][r];
+//     if (result == -1) {
+//       DP[l][r] = min(
+//         play(l+1, r, (turn+1)%m),
+//         play(l, r-1, (turn+1)%m)
+//       );
+//       result = DP[l][r];
+//     }
+//   }
+//   return result;
+// }
 
 int main() {
   std::ios::sync_with_stdio(false);
