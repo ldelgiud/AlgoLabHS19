b92b7372f54b23638cd01106eee76ac19f0faddc
Leonardo Del_Giudice <ldelgiud@student.ethz.ch>
2019-11-25 17:07:59 +0100

    start india

---
+++ potw_10/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+
+void test_case() {
+  int c, g, b, k, a; std::cin >> c >> g >> b >> k >> a;
+  
+
+
+  for (int i = 0; i < g; ++i) {
+    int x, y, d, e; std::cin >> x >> y >> d >> e;
+    
+
+  }
+}
+
+
+int main() {
+  std::ios_base::sync_with_stdio(false);
+  int t; std::cin >> t;
+  while(t--) test_case();
+
+}
