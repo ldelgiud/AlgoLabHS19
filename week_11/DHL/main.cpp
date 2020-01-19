22430981ff9952e453e535925bf37aea865dfaf3
Leonardo Del_Giudice <ldelgiud@student.ethz.ch>
2020-01-19 13:59:31 +0100

    DHL start

---
+++ week_11/DHL/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+int n;
+std::vector<std::vector<int>> DP;
+
+void test_case() {
+
+  std::cin >> n;
+  
+}
+int main() {
+  std::ios_base::sync_with_stdio(false);
+  int t; std::cin >> t;
+  while(t--) test_case();
+
+}
