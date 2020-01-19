8bdc8cc235f0b1e4de63512d20cc6a6ad1980965
Leonardo Del_Giudice <ldelgiud@student.ethz.ch>
2020-01-19 17:04:36 +0100

    DHL 100 points

--- week_11/DHL/main.cpp
+++ week_11/DHL/main.cpp
@@ -2,63 +2,44 @@
 #include <vector>
 #include <deque>
 
-int n;
-std::vector<std::vector<int>> DP;
-std::vector<int> A, B;
-
-int cost(int i, int j, int l, int r) {
-  int sum_l = 0, sum_r = 0;
-  for (int  u = i-1; u >= i-l; --u) {
-    sum_l += A[u];
-  }
-
-  for (int u = j-1; u >= j-r; --u) {
-    sum_r += B[u];
-  }
-  return (sum_l-l)*(sum_r-r);
-}
-
-int play(int i, int j) {
-
-  if (DP[i][j] != -1) return DP[i][j];
-  int min = 1 << 27;
-  for (int l = 1; l <= i; ++l) {
-    for (int r = 1; r <= j; ++r) {
-      int new_i = i-l;
-      int new_j = j-r;
-      if (new_i < 0 || new_j < 0) continue;
-      if (new_i == 0 && new_j == 0) min = std::min(min, cost(i,j,l,r));
-      else {
-	min = std::min(min, play(new_i, new_j) + cost(i,j,l,r));
-      }					   
-    }
-  }
-  DP[i][j] = min;
-  return min;
-}
 void test_case() {
 
-  std::cin >> n;
-  A.clear(); B.clear();
+  int n; std::cin >> n;
+  std::vector<int> A, B;
+  A.push_back(0);
+  B.push_back(0);
   for (int i = 0; i < n; ++i) {
     int a; std::cin >> a;
-    A.push_back(a);
+    A.push_back(a-1);
   }
-
   for (int i = 0; i < n; ++i) {
     int b; std::cin >> b;
-    B.push_back(b);
+    B.push_back(b-1);
   }
 
-  DP.clear();
-  DP = std::vector<std::vector<int>>(n+1, std::vector<int>(n+1, -1));
-  for (int i = 1; i < n; ++i) {
-    DP[i][0] = 1 << 27;
-    DP[0][i] = 1 << 27;
+  std::vector< std::vector<int> > DP(n+1, std::vector<int>(n+1,0));
+  std::vector<long> preA(n+1,0), preB(n+1,0);
+
+  for (int i = 1; i <= n; ++i) {
+    preA[i] = preA[i-1] + A[i];
+    preB[i] = preB[i-1] + B[i];
   }
-  DP[0][0] = 0;
-  int total = play(n,n);
-  std::cout << total << std::endl;
+  
+  for (int i = 1; i <= n; ++i) {
+    DP[1][i] = A[1] * preB[i];
+    DP[i][1] = B[1] * preA[i];
+  }
+
+  for (int i = 2; i <= n; ++i) {
+    for (int j = 2; j <= n; ++j) {
+      DP[i][j] = A[i]*B[j] + std::min(DP[i-1][j-1],
+				      std::min(DP[i][j-1],
+					       DP[i-1][j]));
+
+    }
+  }
+
+  std::cout << DP[n][n] << std::endl;;
 }
 
 
