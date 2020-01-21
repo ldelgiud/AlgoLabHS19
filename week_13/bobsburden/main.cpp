e1a823a2663fe6a9a12975c8cfb88155ac29e1e3
Leonardo Del Giudice <ldelgiud@student.ethz.ch>
2020-01-21 17:27:00 +0100

    bobsburden start

---
+++ week_13/bobsburden/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+void test_case() {
+
+  int k; std::cin >> k;
+
+  for (int i = 1; i <= k; ++i) {
+    for (int j = 0; j < 1; ++j) {
+      int v; std::cin >> v;
+
+    }
+
+  }
+}
+
+int main() {
+  std::ios_base::sync_with_stdio(false);
+  int t; std::cin >> t;
+  while (t--) test_case();
+
+}
+
