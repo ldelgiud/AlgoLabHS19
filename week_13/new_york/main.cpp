187ee3c579eb7e4e9e45b919a13e994b614fd367
Leonardo Del Giudice <ldelgiud@student.ethz.ch>
2020-01-23 13:23:47 +0100

    start new_york

---
+++ week_13/new_york/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+
+
+void test_case() {
+  int n, m, k; std::cin >> n >> m >> k;
+
+  
+}
+
+int main() {
+  std::ios_base::sync_with_stdio(false);
+  int t; std::cin >> t;
+  while (t--) test_case();
+
+}
