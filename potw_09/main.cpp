d6cd7bc1b6443c6699840315ada1e881883a74d4
Leonardo Del Giudice <ldelgiud@student.ethz.ch>
2020-01-26 14:16:09 +0100

    goldeneye 100 points

--- potw_09/main.cpp
+++ potw_09/main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <iomanip>
 
 #include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
 #include <CGAL/Delaunay_triangulation_2.h>
 #include <CGAL/Triangulation_vertex_base_with_info_2.h>
 #include <CGAL/Union_find.h>
+#include <boost/pending/disjoint_sets.hpp>
 
 typedef CGAL::Exact_predicates_inexact_constructions_kernel  K;
 typedef CGAL::Triangulation_vertex_base_with_info_2<int,K>   Vb;
@@ -15,6 +17,7 @@ typedef CGAL::Delaunay_triangulation_2<K,Tds>                Delaunay;
 typedef Delaunay::Finite_edges_iterator                      EI;
 typedef K::Point_2                                           Point;
 typedef std::pair<Point,int>                                 IPoint;
+typedef boost::disjoint_sets_with_storage<>                  Uf;
 
 struct Edge {
   Edge(int u_, int v_, K::FT sql_) : u(u_), v(v_), sql(sql_) {}
@@ -22,13 +25,11 @@ struct Edge {
   K::FT sql;
   
 };
-
 inline bool operator<(const Edge& e, const Edge& f) { return e.sql < f.sql; }
 
 void test_case() {
   long long n, m, p;
   std::cin >> n >> m >> p;
-
   
   std::vector<IPoint> jammers;
   jammers.reserve(n);
@@ -48,23 +49,50 @@ void test_case() {
     edges.push_back(Edge(e->first->vertex((e->second+1)%3)->info(),
 			 e->first->vertex((e->second+2)%3)->info(),
 			 t.segment(e).squared_length()));
-
   }
-  std::sort(edges.begin(), edges.end());
 
-  
+  std::sort(edges.begin(), edges.end());
+  Uf ufp(n);
+  for (auto e : edges) {
+    if (e.sql > p) break;
+    ufp.union_set(e.u, e.v);
+  }
+
+  K::FT a = 0, b = 0;
+  Uf ufa(n), ufb(n);
+  int idx_a = 0, idx_b = 0;
   for (int i = 0; i < m; ++i) {
     int x, y, x_, y_; std::cin >> x >> y >> x_ >> y_;
-    Point s(x,y); Point t(x_, y_);
+    Point src(x,y); Point trg(x_, y_);
+    auto p0 = t.nearest_vertex(src), p1 = t.nearest_vertex(trg);
+    K::FT d = 4*std::max(CGAL::squared_distance(p0->point(), src),
+			 CGAL::squared_distance(p1->point(), trg));
+    if (d <= p && ufp.find_set(p0->info()) == ufp.find_set(p1->info())) {
+      std::cout << 'y';
+      if (d > b) b = d;
+      for (; idx_b < edges.size()
+	     && ufb.find_set(p0->info()) != ufb.find_set(p1->info()); ++idx_b) {
+	ufb.union_set(edges[idx_b].u, edges[idx_b].v);
+      }
+    } else std::cout << 'n';
     
+    if (d > a) a = d;
+    for (; idx_a < edges.size()
+	   && ufa.find_set(p0->info()) != ufa.find_set(p1->info()); ++idx_a) {
+      ufa.union_set(edges[idx_a].u, edges[idx_a].v);
+    }
   }
-
-
+  
+  if (idx_a != 0 && edges[idx_a-1].sql > a) a = edges[idx_a-1].sql;
+  if (idx_b != 0 && edges[idx_b-1].sql > b) b = edges[idx_b-1].sql;
+  std::cout << std::endl << a << std::endl << b << std::endl;
+  
 }
 
 int main() {
+  std::ios_base::sync_with_stdio(false);
+  std::cout << std::fixed << std::setprecision(0);
+
   int t; std::cin >> t;
-
   while (t--) test_case();
-
 }
