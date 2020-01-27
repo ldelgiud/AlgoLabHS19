fe75361cc9c48ca3ba71532b7f9d3a88f034b14b
Leonardo Del Giudice <ldelgiud@student.ethz.ch>
2020-01-27 17:12:01 +0100

    carsharing 100 points

--- potw_13/main.cpp
+++ potw_13/main.cpp
@@ -6,6 +6,8 @@
 // Includes
 // ========
 #include <iostream>
+#include <map>
+
 // BGL includes
 #include <boost/graph/adjacency_list.hpp>
 #include <boost/graph/cycle_canceling.hpp>
@@ -47,56 +49,71 @@ class edge_adder {
 
 struct Request {
   int s, t, d, a, p, idx;
-  Request(int s_, int t_, int d_, int a_, int p_, int idx_) {
-    s = s_; t = t_; d = d_; a = a_; p = p_; idx = idx_;}
+  Request(int s_, int t_, int d_, int a_, int p_) {
+    s = s_; t = t_; d = d_; a = a_; p = p_;}
 };
 
 inline bool operator< (const Request& l, const Request& r) {return l.d < r.d;};
 //inline bool operator< (const int l, const Request& r) {return l < r.d;};
 
-#define trace(x) //std::cout << #x << " = " << x << std::endl
-
+#define trace(x)// std::cerr << #x << " = " << x << std::endl
+#define step(x) //std::cerr << "STEP: " << x << std::endl
 void test_case() {
-  int N, S; std::cin >> N >> S;
-
-  graph G(N+S);
-  const int src = boost::add_vertex(G);
-  const int trg = boost::add_vertex(G);
-  edge_adder adder(G);
   
+  int N, S; std::cin >> N >> S;
+  graph G;
+  edge_adder adder(G);
+  std::vector<int> cars; int max_cars = 0;
   for (int i = 0; i < S; ++i) {
     int l; std::cin >> l;
-    adder.add_edge(src, i, l, 0);
-    adder.add_edge(i, trg, l, 0);
+    cars.push_back(l);
+    max_cars += l;
   }
-
-  std::vector<std::multiset<Request>> R(S);
+  
+  std::vector<std::set<int>> R(S);
+  std::vector<Request> req;
+  req.reserve(N);
+  int max_time = 0;
   for (int i = 0; i < N; ++i) {
     int s, t, d, a, p; std::cin >> s >> t >> d >> a >> p;
-    R[s-1].insert(Request(s-1,t-1,d,a,p,i));
- 
+    R[s-1].insert(d);
+    R[t-1].insert(a);
+    req.push_back(Request(s-1,t-1,d,a,p));
+    max_time = std::max(max_time, a);
   }
-  int cnt = 0;
+
+  //maps time to vertex (=TTV)
+  std::vector<std::map<int,int>> ttv(S);
   for (int i = 0; i < S; ++i) {
+    R[i].insert(0);
+    R[i].insert(max_time);
     for (auto it = R[i].begin(); it != R[i].end(); ++it) {
-      adder.add_edge(it->s, S+it->idx, 1, -it->p);
-      adder.add_edge(S+it->idx, trg, 1, 0);
-      
-      Request tmp(1,1, it->a, it->a, 1, 1);
-      
-      for (auto it2 = R[it->t].lower_bound(tmp); it2 != R[it->t].end(); ++it2) {
-	adder.add_edge(S+it->idx, S+it2->idx, 1, -it2->p);
-	trace(cnt++);
-      }
+      ttv[i].insert({*it, boost::add_vertex(G)});
     }
   }
-  trace(cnt);
-  // Option 1: Min Cost Max Flow with cycle_canceling
+  const int src = boost::add_vertex(G);
+  const int trg = boost::add_vertex(G);
+
+  for (int i = 0; i < S; ++i) {
+    auto it = R[i].begin();
+    adder.add_edge(src, ttv[i][*it], cars[i], 0);
+    for (; std::next(it) != R[i].end(); ++it) {
+      adder.add_edge(ttv[i][*it], ttv[i][*std::next(it)], max_cars, 100*(*std::next(it)-*it));
+    }
+    adder.add_edge(ttv[i][*it], trg, max_cars, 0);
+  }
+
+  for (auto r : req) {
+    int start = ttv[r.s][r.d];
+    int end = ttv[r.t][r.a];
+    adder.add_edge(start, end, 1, 100*(r.a-r.d)-r.p);
+  }
+
+  long offset = 100*max_time*max_cars;
   int flow1 = boost::push_relabel_max_flow(G, src, trg);
-  boost::cycle_canceling(G);
-  int cost1 = boost::find_flow_cost(G);
-  std::cout << -cost1 << "\n"; 
-  trace(cnt);
+  boost::successive_shortest_path_nonnegative_weights(G, src, trg);
+  int cost = boost::find_flow_cost(G);
+  std::cout << offset - cost << "\n"; 
 }
 
 
