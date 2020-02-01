a7a34de473032c46953cd083551ff889c50038e3
Leonardo Del Giudice <ldelgiud@student.ethz.ch>
2020-02-01 19:06:05 +0100

    rewrite algocoon

--- week_09/algocoon/main.cpp
+++ week_09/algocoon/main.cpp
@@ -13,7 +13,6 @@
 #include <algorithm>
 #include <vector>
 #include <queue>
-#include <limits>
 // BGL includes
 #include <boost/graph/adjacency_list.hpp>
 #include <boost/graph/push_relabel_max_flow.hpp>
@@ -43,111 +42,69 @@ class edge_adder {
   void add_edge(int from, int to, long capacity) {
     auto c_map = boost::get(boost::edge_capacity, G);
     auto r_map = boost::get(boost::edge_reverse, G);
-    bool success; edge_desc e;
-    boost::tie(e, success) = boost::add_edge(from, to, G);
-    if (success) {
-      const edge_desc rev_e = boost::add_edge(to, from, G).first;
-      c_map[e] = capacity;
-      c_map[rev_e] = 0; // reverse edge has no capacity!
-      r_map[e] = rev_e;
-      r_map[rev_e] = e;
-    } else {
-      c_map[e] += capacity;
-    }
+    const edge_desc e = boost::add_edge(from, to, G).first;
+    const edge_desc rev_e = boost::add_edge(to, from, G).first;
+    c_map[e] = capacity;
+    c_map[rev_e] = 0; // reverse edge has no capacity!
+    r_map[e] = rev_e;
+    r_map[rev_e] = e;
   }
 };
 
-#define trace(x) std::cout << #x << ": " << x << std::endl;
-const int INF = std::numeric_limits<int>::max();
 void test_case() {
   int n, m; std::cin >> n >> m;
 
   graph G(n);
-
   edge_adder adder(G);
   auto rc_map = boost::get(boost::edge_residual_capacity, G);
   
   for (int i = 0; i < m; ++i) {
-    int a, b, c;
-    std::cin >> a >> b >> c;
+    int a, b, c; std::cin >> a >> b >> c;
     adder.add_edge(a, b, c);
   }
 
-  // Find a min cut via maxflow
-  int best_flow = INF;
-  int cnt;
-  std::vector<bool> vis;  
-  
-  for (int i = 1; i < n; ++i) {
-    int flow = boost::push_relabel_max_flow(G, 0, i);
-
-    if (flow < best_flow) {
-      best_flow = flow;
-      cnt = 1;
-      // BFS to find vertex set S
-      vis = std::vector<bool>(n, false); // visited flags
-      std::queue<int> Q; // BFS queue (from std:: not boost::)
-      vis[0] = true; // Mark the source as visited
-      Q.push(0);
-      while (!Q.empty()) {
-	const int u = Q.front();
-	Q.pop();
-	out_edge_it ebeg, eend;
-	for (boost::tie(ebeg, eend) = boost::out_edges(u, G); ebeg != eend; ++ebeg) {
-	  const int v = boost::target(*ebeg, G);
-	  // Only follow edges with spare capacity
-	  if (rc_map[*ebeg] == 0 || vis[v]) continue;
-	  vis[v] = true;
-	  cnt++;
-	  Q.push(v);
-	}
-      } 
-    }
-      
-  }
-
-   for (int i = 1; i < n; ++i) {
-    int flow = boost::push_relabel_max_flow(G, i, 0);
-
-    if (flow < best_flow) {
-      best_flow = flow;
-      cnt = 1;
-      // BFS to find vertex set S
-      vis = std::vector<bool>(n, false); // visited flags
-      std::queue<int> Q; // BFS queue (from std:: not boost::)
-      vis[i] = true; // Mark the source as visited
-      Q.push(i);
-      while (!Q.empty()) {
-	const int u = Q.front();
-	Q.pop();
-	out_edge_it ebeg, eend;
-	for (boost::tie(ebeg, eend) = boost::out_edges(u, G); ebeg != eend; ++ebeg) {
-	  const int v = boost::target(*ebeg, G);
-	  // Only follow edges with spare capacity
-	  if (rc_map[*ebeg] == 0 || vis[v]) continue;
-	  vis[v] = true;
-	  cnt++;
-	  Q.push(v);
-	}
-      } 
-    }
-      
-  }
-  
-  
-  
-  
-  std::cout << best_flow << std::endl << cnt << ' ';
+  int min_flow = 1 << 29; int idx = -1;
   for (int i = 0; i < n; ++i) {
-    if (vis[i]) std::cout << i << ' ';
+    int flow = boost::push_relabel_max_flow(G, i, (i+1)%n);
+    if (min_flow > flow) {
+      min_flow = flow;
+      idx = i;
+    }
+  }
+  
+  boost::push_relabel_max_flow(G, idx, (idx+1)%n);
+  std::cout << min_flow << std::endl;
+
+  // BFS to find vertex set S
+  int figurines = 1;
+  std::vector<int> vis(n, false); // visited flags
+  std::queue<int> Q; // BFS queue (from std:: not boost::)
+  vis[idx] = true; // Mark the source as visited
+  Q.push(idx);
+  while (!Q.empty()) {
+    const int u = Q.front();
+    Q.pop();
+    out_edge_it ebeg, eend;
+    for (boost::tie(ebeg, eend) = boost::out_edges(u, G); ebeg != eend; ++ebeg) {
+      const int v = boost::target(*ebeg, G);
+      // Only follow edges with spare capacity
+      if (rc_map[*ebeg] == 0 || vis[v]) continue;
+      vis[v] = true;
+      ++figurines;
+      Q.push(v);
+    }
+  }
+  std::cout << figurines << ' ';
+  // Output S
+  for (int i = 0; i < n; ++i) {
+    if (vis[i]) std::cout << i << " ";
   }
   std::cout << "\n";
-  
-  
+	
 }
 
-
 int main() {
+  std::ios_base::sync_with_stdio(false);
   int t; std::cin >> t;
-  while(t--) test_case();
+  while (t--) test_case();
 }
