dbec5d45042088e14922d83c9118ec91eaa39117
Leonardo Del Giudice <ldelgiud@student.ethz.ch>
2020-02-02 15:15:51 +0100

    motorcycles 60 points

--- potw_04/main.cpp
+++ potw_04/main.cpp
@@ -2,76 +2,74 @@
 #include <algorithm>
 #include <vector>
 #include <utility>
+#include <queue>
 
 #include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
 #include <CGAL/Ray_2.h>
 typedef CGAL::Exact_predicates_inexact_constructions_kernel   K;
 typedef K::Ray_2 Ray;
-typedef K::Point_2 Point;
+typedef K::Point_2 P;
+// {(idx, y_0), (x_1, y_1)}
+typedef std::pair<P,P> Biker;
+
+K::FT speed(const Biker b) {
+  return std::abs((b.first.y()-b.second.y())/b.second.x());
+}
+
+bool hits(const Biker l, const Biker r) {
+  Ray left(P(0,l.first.y()),l.second);
+  Ray right(P(0,r.first.y()),r.second);
+  return CGAL::do_intersect(left, right);
+}
+inline bool cmp_biker(const Biker l, const Biker r) {
+  return l.first.y() < r.first.y();
+}
 
 void test_case() {
   int n; std::cin >> n;
-  //biker, active
-  std::vector<std::pair<Ray, int>> bikers;
-  bikers.reserve(n);
-    
+
+  std::vector<Biker> B;
+  B.reserve(n);
   for (int i = 0; i < n; ++i) {
     long y_0, x_1, y_1;
     std::cin >> y_0 >> x_1 >> y_1;
-    bikers.push_back(std::make_pair(Ray(Point(0,y_0), Point(x_1, y_1)),i));
+    B.push_back({P(i,y_0),P(x_1,y_1)});
   }
   
-  std::sort(bikers.begin(), bikers.end(), [](std::pair <Ray,int> a, std::pair<Ray,int>b) {
+  std::sort(B.begin(), B.end(), cmp_biker);
+  std::deque<Biker> S;
+  int idx = 0;
 
-      Ray a1 = a.first;
-      Ray b1 = b.first;
-      double angle_a = std::atan(CGAL::to_double(
-			       a1.point(1).x()/(a1.point(1).y()-a1.source().y())));
-      double angle_b = std::atan(CGAL::to_double(
-			       b1.point(1).x()/(b1.point(1).y()-b1.source().y())));
-
-      if (angle_a > std::atan(1)*4) angle_a -= std::atan(1)*4;
-      if (angle_b > std::atan(1)*4) angle_b -= std::atan(1)*4;
-      
-      if (angle_a < angle_b) {
-	return false;
-      } else if
-	  (angle_a  == angle_b) {
-	return !(a1.source().y() < b1.source().y()); 
-      } else return true;
-    });
-
-  
-  std::vector<bool> alive(n, true);
-  for (int i = 0; i < n; ++i) {
-    if (alive[i]) {
-      for (int j = i+1; j < n; ++j) {
-	if (alive[j]) {
-	  if (CGAL::do_intersect(bikers[i].first,bikers[j].first)) alive[j] = false;
-	  
+  while (idx < n) {
+    Biker curr = B[idx++];
+    while (!S.empty()) {
+      Biker opponent = S.front();
+      if (hits(curr, opponent)) {
+	if (speed(opponent) < speed(curr)) {
+	  break;
+	} else {
+	  S.pop_front();
 	}
-      }
-    }
+      } else {
+	S.push_front(curr);
+	break;
+      } 
+    } if (S.empty()) S.push_back(curr);
   }
 
-  
-  int cnt = -1;
-  std::vector<int> res;
-  for (bool i: alive) {
-    ++cnt;
-    if (i) res.push_back(bikers[cnt].second);
+  std::vector<int> survivors;
+  for (Biker b : S) {
+    survivors.push_back(b.first.x());
   }
-
-  std::sort(res.begin(), res.end());
-
-  for (int i: res) std::cout << i << ' ';
+  std::sort(survivors.begin(), survivors.end());
+  for (int i : survivors) {
+    std::cout << i << ' ';
+  }
   std::cout << std::endl;
 }
-
 
 int main() {
   std::ios::sync_with_stdio(false);
   int t; std::cin >> t;
   while(t--) test_case();
-  
 }
